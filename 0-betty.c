8aaf389e21de4a358d2581a81b26b12925388649
Damilola Bakare <adamilola414@gmail.com>
2022-05-18 02:31:49 -0700

    a code that passes the betty checks

--- 0-betty.c
+++ 0-betty.c
@@ -1,40 +1,13 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * main - entry point for application
- * @ac: argument count
- * @av: argument vector
- * Return: 0 on success
+ * main - entry point
+ * Description: code that passes Betty checks
+ * Return: Always 0
  */
-int main(int ac, char **av)
+
+int main(void)
 {
-	config build;
-	(void)ac;
-	signal(SIGINT, handle_sigint);
-	config_init(&build);
-	build.shell_name = av[0];
-	shell(&build);
+	printf("This code passes the Betty checks!\n");
 	return (0);
-}
-
-/**
- * config_init - initialize member values for config struct
- * @build: input build
- *
- * Description: Accepts structured input commnad, and finds the right
- * configuration to execute from the built-in configurations.
- * Return: build with initialized members
- */
-config *config_init(config *build)
-{
-	build->env = generateLinkedList(environ);
-	build->env_list = NULL;
-	build->args = NULL;
-	build->buffer = NULL;
-	build->path = _getenv("PATH", environ);
-	build->full_path = NULL;
-	build->count_line = 0;
-	build->shell_name = NULL;
-	build->error_status = 0;
-	return (build);
 }
